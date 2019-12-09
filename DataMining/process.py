9e54935b5ede9a6d0f35ff54718a615328c37af4
mixianxian <lixiangthu15@qq.com>
2019-12-09 16:20:29 +0800

    add word2vec function

--- DataMining/process.py
+++ DataMining/process.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import jieba
+import gensim
 
 # Read csv file as numpy array
 def csv_read(path):
@@ -15,15 +16,44 @@ def csv_read(path):
 def split():
 	pass
 
+# word segmentation and save result
 def jieba_fenci(doc):
-	seg_list = [list(jieba.cut(sentence)) for sentence in doc[:100]]
-
-	# To save it properly
-	seg_save = [str(item)[1:-1]+'\n' for item in seg_list]
+	fenci = []
 	with open('fenci.txt','w',encoding='utf-8') as f:
-		f.writelines(seg_save)
-	return seg_list
+		for sentence in doc:
+			seg_list = list(jieba.cut(sentence))
+			f.write(str(item)[1:-1]+'\n')
+			fenci.append(seg_list)
+	return fenci
+
+# Try to use iterator to save memory
+# which is probably unnecessary in this case
+class Mysentences:
+	"""docstring for Mysentences"""
+	def __init__(self, filepath):
+		self.fielpath = filepath
+
+	def __iter__(self):
+		for line in open(filepath):
+			yield line.split()
+
+# Training word2vec model and save result
+def word2vec():
+	sentences = Mysentences('data/training.csv')
+	for i in range(5):
+		print(sentences)
+	model = gensim.model.word2vec(sentences,min_count=10,size=200,workers=4)
+	modle.save('data/mymodel')
+
+	'''
+	# Save word vector
+	with open('WordVector.txt','w') as f:
+		for word in model.wv.vocab:
+			f.write(word+str(model.wv[word]))
+	'''
+		
+def main():
+	text,label = csv_read('data/train.csv')
+	seg_text = jieba_fenci(text)	
 
 if __name__ == '__main__':
-	text,label = csv_read('data\\train.csv')
-	seg_list = jieba_fenci(text)
